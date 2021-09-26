0757e697a45d4578ec804c35aeec99d29d9dfa03
Your Name <you@example.com>
2021-09-26 21:36:03 +0300

    alldone

---
+++ test.sh
@@ -0,0 +1,13 @@
+go run main.go "banana standard abc"
+go run main.go "hello" standard
+go run main.go "hello world" shadow
+go run main.go "nice 2 meet you" thinkertoy
+go run main.go "you & me" standard
+go run main.go "123" shadow
+go run main.go "/(\")" thinkertoy
+go run main.go "ABCDEFGHIJKLMNOPQRSTUVWXYZ" shadow
+go run main.go "\"#$%&/()*+,-./" thinkertoy
+go run main.go "It's Working" thinkertoy
+go run main.go "Testing" shadow
+go run main.go "112" standard
+go run main.go "RGB" thinkertoy
